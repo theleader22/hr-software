5fb6291695669b6408fe013f5017744d5df196a9
mynk10 <mynk1008@gmail.com>
2025-11-19 13:59:47 +0530

    #2 backend files

---
+++ public/backend/assets/js/dashboards-analytics.js
@@ -0,0 +1,24 @@
+/**
+ * Dashboard Analytics
+ */
+
+'use strict';
+
+(function () {
+  // swiper loop and autoplay
+  // --------------------------------------------------------------------
+  const swiperWithPagination = document.querySelector('#swiper-with-pagination-cards');
+  if (swiperWithPagination) {
+    new Swiper(swiperWithPagination, {
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+      },
+      pagination: {
+        clickable: true,
+        el: '.swiper-pagination'
+      }
+    });
+  }
+})();
